b82f813a7bcb8679ea2e0113a008a0bd47bacb74
pmme <gpaulmcs@gmail.com>
2020-03-21 19:08:36 +1300

    Support EpicSpawners

--- src/main/java/nz/pmme/nospawnerforafk/listeners/EventsListener.java
+++ src/main/java/nz/pmme/nospawnerforafk/listeners/EventsListener.java
@@ -2,13 +2,11 @@ package nz.pmme.nospawnerforafk.listeners;
 
 import nz.pmme.nospawnerforafk.NoSpawnerForAfk;
 import org.bukkit.Location;
-import org.bukkit.block.CreatureSpawner;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.SpawnerSpawnEvent;
 
 import java.util.Collection;
 
@@ -20,10 +18,32 @@ public class EventsListener implements Listener
     }
 
     @EventHandler
-    public void onSpawnerSpawn( SpawnerSpawnEvent event )
+    public void onSpawnerSpawnSpigot( org.bukkit.event.entity.SpawnerSpawnEvent event )
     {
         int range = this.plugin.getConfig().getInt( "check-range" );
-        CreatureSpawner spawner = event.getSpawner();
+        org.bukkit.block.CreatureSpawner spawner = event.getSpawner();
+        Location spawnerLocation = spawner.getLocation();
+        Collection<Entity> nearbyEntities = spawnerLocation.getWorld().getNearbyEntities( spawnerLocation, range, range, range, (e)->(e.getType()==EntityType.PLAYER) );
+
+        boolean allPlayersAfk = true;
+        for( Entity entity : nearbyEntities ) {
+            if( entity instanceof Player ) {
+                Player player = ( Player )entity;
+                net.ess3.api.IUser essentialsUser = plugin.getEssentials().getUser( player );
+                if( !essentialsUser.isAfk() || player.hasPermission( "nospawnerforafk.bypass" ) ) {
+                    allPlayersAfk = false;
+                    break;
+                }
+            }
+        }
+        if( allPlayersAfk ) event.setCancelled( true );
+    }
+
+    @EventHandler
+    public void onSpawnerSpawnEpicSpawners( com.songoda.epicspawners.api.events.SpawnerSpawnEvent event )
+    {
+        int range = this.plugin.getConfig().getInt( "check-range" );
+        com.songoda.epicspawners.spawners.spawner.Spawner spawner = event.getSpawner();
         Location spawnerLocation = spawner.getLocation();
         Collection<Entity> nearbyEntities = spawnerLocation.getWorld().getNearbyEntities( spawnerLocation, range, range, range, (e)->(e.getType()==EntityType.PLAYER) );
 
